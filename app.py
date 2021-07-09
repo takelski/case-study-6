7db91d9c67a516d75cc1094d7b4e4c55af159d10
takelski <87156720+takelski@users.noreply.github.com>
2021-07-09 11:24:22 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,52 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import pandas as pd
 import dash
-
-app = dash.Dash(__name__)
+from app import app, server
+from apps import dashboard, dashboard2
+import sqlite3
+from dash.exceptions import PreventUpdate
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
-app.title = 'Plotly Dash Multipage Template App'
+
+csv = "mydatastore.csv"
+# Add global app methods here
+def get_database():
+
+    db = pd.read_csv(csv)
+
+    return db
+
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+
+    html.Div(children=[
+        html.H1(children='Plotly Dash Multipage Template App'),
+        html.Div(children=[
+            html.A(html.Button('Dashboard', className='tab-button'),
+                href='/'),
+            html.A(html.Button('Dashboard2', className='tab-button'),
+                href='/dashboard2'),
+        ], className='tabs'),
+    ], className='nav'),
+
+    html.Div(id='page-content'),
+])
+
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+
+    df = get_database()
+
+    if pathname == '/':
+        return dashboard.get_dashboard_layout(df)
+    elif pathname == '/dashboard2':
+         return dashboard2.get_dashboard2_layout(df)
+    else:
+        return dashboard.get_dashboard_layout(df)
+
+if __name__ == '__main__':
+    app.run_server()
